e5bfd815f4cc6ba534f1d642c8f1f2faee622f6b
Bryan Pedraza <Bryanpredrazay57@icloud.com>
2024-02-17 20:24:55 -0800

    Third_commit

--- website/views.py
+++ website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from posts.models import Posts
 
 
 
 # Create your views here.
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"current_time": datetime.now(),
+                   "posts": Posts.objects.all(),
+                   "num_posts": Posts.objects.count()})
