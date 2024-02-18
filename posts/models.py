25f1b86ce62e95691855c0ec85f61da2580a9355
Bryan Pedraza <Bryanpredrazay57@icloud.com>
2024-02-17 19:41:23 -0800

    Second_commit

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from typing import Any
+from django.db import models
+
+
+# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=25)
+    body = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return f'{self.title}'
